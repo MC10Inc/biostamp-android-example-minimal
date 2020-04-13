a5f3e098c033e2de53dd1799b02119e3b6f7614a
Paolo DePetrillo <pdepetrillo@mc10inc.com>
2020-04-13 15:37:28 -0400

    Request permissions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# biostamp-android-example-minimal
-Minimal example app using Android SDK
+# Minimal Example App for BioStamp3™ Android SDK
+
+This is a minimal example application that demonstrates how to use the
+[BioStamp3™ Android SDK][1].
+
+[1]: https://github.com/MC10Inc/biostamp-android
+
--- app/src/main/java/com/mc10inc/biostamp3/minimalexample/MainActivity.java
+++ app/src/main/java/com/mc10inc/biostamp3/minimalexample/MainActivity.java
@@ -1,14 +1,72 @@
 package com.mc10inc.biostamp3.minimalexample;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mc10inc.biostamp3.minimalexample.databinding.ActivityMainBinding;
+import com.mc10inc.biostamp3.sdk.BioStampManager;
 
 public class MainActivity extends AppCompatActivity {
+    // Code to identify the response to our permissions request in onPermissionsRequestResult
+    private static final int PERMISSIONS_REQUEST_FOR_BLE = 1;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.scanButton.setOnClickListener(this::scanClicked);
+    }
+
+    private void scanClicked(View view) {
+        // Before we can scan for sensors, we must check if our app has been granted the necessary
+        // permissions by the end user, and request the permissions if not.
+        BioStampManager bs = BioStampManager.getInstance();
+        if (bs.hasPermissions()) {
+            scanForSensors();
+        } else {
+            requestBlePermissions();
+        }
+    }
+
+    public void requestBlePermissions() {
+        // Request the permissions that are needed to scan for sensors. The user will see a popup
+        // asking them to grant the permissions, and then Android will call
+        // onRequestPermissionsResult with the result.
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                PERMISSIONS_REQUEST_FOR_BLE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_FOR_BLE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // The user granted the permissions; now we can start scanning.
+                scanForSensors();
+            } else {
+                new AlertDialog.Builder(this)
+                        .setMessage("Permissions were not granted. Cannot scan for sensors.")
+                        .create()
+                        .show();
+            }
+        }
+    }
+
+    private void scanForSensors() {
+
     }
 }
